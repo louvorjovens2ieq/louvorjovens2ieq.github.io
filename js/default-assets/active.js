cd266f111291f214c59d236787ce4fdce9327afb
Leandro Pompélia <leandro.pompelia@fatec.sp.gov.br>
2019-05-15 01:08:31 -0300

    Ajustes

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -114,7 +114,7 @@
   // ***********************************
   if($.fn.scrollUp) {
     poca_window.scrollUp({
-      scrollSpeed: 2000,
+      scrollSpeed: 20000,
       scrollText: '<i class="fa fa-angle-up"</i>'
     });
   }
