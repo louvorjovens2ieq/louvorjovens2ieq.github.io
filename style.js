b544977bbbf150164ad92ce6a703c20c03d3b2af
Leandro Pompélia <leandro.pompelia@fatec.sp.gov.br>
2019-05-20 19:33:01 -0300

    Ajustes

--- style.js
+++ style.js
@@ -28,3 +28,32 @@
     };
     
 } )();
+
+( function() {
+
+    var modalCifra = document.querySelectorAll( ".modalCifra" );
+    var destin_modal_cifra = document.querySelectorAll(".modal_cifra") 
+    
+    for (var i = 0; i < modalCifra.length; i++) {
+        
+        // var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+
+        modalCifra[i].addEventListener( "click", function() {
+
+            var iframe = document.createElement( "iframe" );
+
+                    iframe.setAttribute( "frameborder", "0" );
+                    iframe.setAttribute( "width", "100%" );
+                    iframe.setAttribute( "height", "500px" );
+                    iframe.setAttribute( "scrolling", "auto" );
+
+                    iframe.setAttribute( "src", "https://www.cifraclub.com.br/"+ this.dataset.embed );
+
+                    // alert(this.dataset.embed);
+
+                    document.querySelector(".modal_cifra").innerHTML = "";
+                    document.querySelector(".modal_cifra").appendChild( iframe );
+        } );    
+    };
+    
+} )();
